7b67fb0d7ead286b7df8990735fae4bc4bd5f3af
U-VJHD2LVVGM0FDZ7\Administrator <308515245@qq.com>
2018-11-08 18:57:30 +0800

    m

--- bounce2d.c
+++ bounce2d.c
@@ -1,4 +1,23 @@
 #include "curses.h"
 #include "signal.h"
 #include "bounce.h"
+#include "stdio.h"
 
+struct ppball ball;
+void setup();
+void wrapUp();
+
+int main(){
+	int c;
+	setUp();
+	while((c=getchar())!='q'){
+		switch (c){
+			case 'f':ball.xTtm--;break;
+			case 'g':ball.yTtm--;break;
+			case 's':ball.xTtm++;break;
+			case 'd':ball.yTtm++;break;
+		}
+	}
+	wrapUp();
+}
+// setUp,wrapUp
