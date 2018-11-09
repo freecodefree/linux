19fdf84bdc2da56958a5d67f1aaf92ee56882e1c
U-VJHD2LVVGM0FDZ7\Administrator <308515245@qq.com>
2018-11-09 11:20:51 +0800

    bounce or lose

--- bounce2d.c
+++ bounce2d.c
@@ -6,6 +6,8 @@
 struct ppball ball;
 void setup();
 void wrapUp();
+void moveBall(int);
+int bounceOrLose(struct ppball *);
 
 int main(){
 	int c;
@@ -20,4 +22,53 @@ int main(){
 	}
 	wrapUp();
 }
+void setUp(){
+	ball.xPos=X_INIT;
+	ball.yPos=Y_INIT;
+	ball.xTtg=ball.xTtm=X_TTM;
+	ball.yTtg=ball.yTtm=Y_TTM;
+	ball.xDir=1;
+	ball.yDir=1;
+	ball.symbols=DFL_SYMBOL;
+
+	initscr();
+	noecho();
+	crmode();
+//	signal(SIGINT,SIG_IGN);
+	signal(SIGALRM,moveBall);
+	setTicker(TICKS_PER_SEC/50);	
+//	mvaddch(ball.yPos,ball.xPos,ball.symbols);
+//	move(LINS,COLS);
+
+}
+
+void moveBall(int s){
+	int yCur,xCur,moved;
+	signal(SIGALRM,SIG_IGN);
+	moved=0;
+
+	if(ball.xTtm>0&&ball.xTtg--==1){
+		ball.xTtg=ball.xTtm;
+		xCur=ball.xPos;
+		ball.xPos+=ball.xDir;
+		moved=1;
+	}
+	if(ball.yTtm>0&&ball.yTtg--==1){
+                ball.yTtg=ball.yTtm;
+                yCur=ball.yPos;
+                ball.yPos+=ball.yDir;
+                moved=1;
+        }
+	if(moved){
+		mvaddch(yCur,xCur,BLANK);
+		mvaddch(ball.yPos,ball.xPos,ball.symbols);
+		move(LINES,COLS);
+		refresh();
+		bounceOrLose(&ball);
+	}
+	signal(SIGALRM,moveBall);
+
+}
+// yCur,xCur,moved,
+// initscr,noecho,crmode,SIG_IGN,mvaddch,refresh,setTicker
 // setUp,wrapUp
