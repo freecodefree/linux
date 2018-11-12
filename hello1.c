d608e433d7fecd5fde2bcddc6819890d90e7325e
U-SHENZHOU\Tree <308515245@qq.com>
2018-11-12 11:24:32 +0800

    hello1

--- hello1.c
+++ hello1.c
@@ -1,14 +1,31 @@
 #include "stdio.h"
 #include "curses.h"
+//#include "stdlib.h"
+#include "unistd.h"
 
-int main(){
+int main(int ac,char **av){
+	char *s;
 	initscr();
 	clear();
 	move(10,10);
-	addstr("hello worle");
-	move(LINES-1,1);
-	refresh();
-	getch();
+	if(ac==2){
+		s=*++av;
+	}else{
+		s="hello worle";
+	}
+	while(1){
+	        move(10,10);
+		addstr(s);
+		move(0,0);
+		refresh();
+		sleep(1);
+		clear();
+		refresh();
+		sleep(1);
+	}
+//	move(LINES-1,1);
+//	refresh();
+//	getch();
 	endwin();
 	
 }
