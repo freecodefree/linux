c4aac25e5965ac3bfcbd28c91f514b214e9e45d9
U-VJHD2LVVGM0FDZ7\Administrator <308515245@qq.com>
2018-11-22 16:41:15 +0800

    get full

--- tickerTape.c
+++ tickerTape.c
@@ -3,6 +3,8 @@
 #include "stdlib.h"
 #include "curses.h"
 
+#define BUFSIZE 1024
+
 struct TAPE{
 	int x,y,len;
 	char *s;
@@ -21,8 +23,26 @@ int main(int ac,char **av){
 }
 
 int getXYLens(char **av,struct TAPE *tape){
+	int retval=0;
 	tape->x=atoi(av[1]);
 	tape->y=atoi(av[2]);
 	tape->len=atoi(av[3]);
-	return 0;
+
+	if(tape->x<0||tape->x>=(COLS-1)){
+		tape->x=0;
+		retval=1;
+	}
+	if(tape->y<0||tape->y>=(LINES-1)){
+		tape->y=0;
+		retval=1;
+	}
+	if(tape->len<=0||tape->len>(COLS-1-tape->x)){
+		tape->len=COLS-1-tape->x;
+		retval=1；
+	}
+	return retval;
+}
+
+int shouTape(struct TAPE *tape,char *filename){
+	char buff[BUFSIZE];
 }
