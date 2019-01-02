605c1ecfa76841b5e0d61ec706e45b55ddfc60cb
U-VJHD2LVVGM0FDZ7\Administrator <308515245@qq.com>
2019-01-02 15:05:16 +0800

    process

--- smsh2.c
+++ smsh2.c
@@ -7,6 +7,12 @@
 
 #define DFL_PROMPT "> "
 void setup();
+void fatal();
+int is_control_command(char *);
+int do_control_command(char **);
+int ok_to_execute();
+int process(char **);
+
 int main(){
 	char *cmdline,*prompt,**arglist;
 	int result=0,process(char **);
@@ -24,4 +30,27 @@ int main(){
 	}
 	return 0;
 }
+
+void setup(){
+	signal(SIGINT,SIG_IGN);
+	signal(SIGQUIT,SIG_IGN);
+}
+
+void fatal(char *s1,char *s2,int n){
+	fprintf(stderr,"Error:%s,%s\n",s1,s2);
+	exit(n);
+}
+
+int process(char **args){
+	int rv=0;
+	if(args[0]==NULL){
+		return rv;
+	}else if(is_control_command(args[0])){
+		rv=do_control_command(args);
+	}else if(ok_to_execute()){
+		rv=execute(args);
+	}
+	return rv;
+}
+// 
 // cmdline,prompt,arglist,result,process,setup,nextCmd,splitline,freelist,free
