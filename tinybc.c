98775578bcaa60da88c06a4b07e13bc300754cc5
U-SHENZHOU\Tree <308515245@qq.com>
2019-01-10 10:15:30 +0800

    be_dc

--- tinybc.c
+++ tinybc.c
@@ -20,4 +20,23 @@ int main(){
 		wait(NULL);
 	}
 }
+
+void be_dc(int in[],int out[]){
+	if(dup2(in[0],0)==-1){
+		oops("dc:cannot redirect stdin",2);
+	}
+	close(in[0]);
+	close(in[1]);
+
+	if(dup2(out[1],1)==-1){
+		oops("dc:cannot redirect stdout",2);
+	}
+	close(out[0]);
+	close(out[1]);
+
+	execlp("dc","dc","-",NULL);
+	oops("fail to execute dc",2);
+	
+}
+// execlp
 // pid,todc[2],fromdc,pipe,be_dc,be_bc,wait
