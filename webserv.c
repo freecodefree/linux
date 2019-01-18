b9a34a532e28cb5b462742e065853c3fa19ccd09
U-SHENZHOU\Tree <308515245@qq.com>
2019-01-18 16:03:55 +0800

    process_rq

--- webserv.c
+++ webserv.c
@@ -1,6 +1,7 @@
 #include "socklib.c"
 #include "sys/types.h"
 #include "sys/stat.h"
+#include "string.h"
 
 int main(int ac,char **av){
 	int sock,fd;
@@ -38,4 +39,29 @@ int main(int ac,char **av){
 		fclose(fpin);
 	}
 }
+
+void read_til_crnl(FILE *fp){
+	char buf[BUFSIZ];
+	while(fgets(buf,BUFSIZ,fp)!=NULL&&strcmp(buf,"\r\n")!=0);
+}
+
+void process_rq(char *rq,int fd){
+	char cmd[11],arg[513];
+	if(fork()!=0)return;
+	strcpy(arg,"./");
+	if(scanf(rq,"%10s%512s",cmd,(arg+2))!=2)return;
+	if(strcmp(cmd,"GET")!=0){
+		cannot_do(fd);
+	}else if(not_exist(arg)){
+		do_404(arg,fd);
+	}else if(isadir(arg)){
+		do_ls(arg,fd);
+	}else if(ends_in_cgi(arg)){
+		do_exec(arg,fd);
+	}else{
+		do_cat(arg,fd);
+	}
+}
+// rq,fd,cmd,arg,cannot_do,not_exist,do_404,isadir,do_ls,ends_in_cgi,do_exec
+// do_cat
 // sock,fd,fpin,request,read_til_crnl,process_rq
